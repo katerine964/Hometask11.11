f53ce2df95b00c2176c23c1c557a416124c510f0
Kate <ekatromanova90@mail.ru>
2022-11-13 01:45:23 +0300

    T66

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Sum(int m, int n)
+{
+    if (n == m) return n;
+ 
+    return n+Sum(m, n-1);
+}
+Console.Write("Введите число M: "); 
+int m = Convert.ToInt32(Console.ReadLine());  
+Console.Write("Введите число N: "); 
+int n = Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine($"Cумма чисел между числами {m} и {n} = {Sum(m,n)}");
+

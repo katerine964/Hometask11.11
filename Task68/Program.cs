7b708f3ab7ff3d4017ae70a0fba8469aa84bd28a
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-11-13 02:38:13 +0300

    Add files via upload

---
+++ Task68/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Akkerman(int a, int b)
+{
+    if (a==0)
+        return b+1;
+    else
+        {
+        if(a>0 && b==0)
+            return Akkerman((a-1),1);
+        else
+            return Akkerman(a-1,Akkerman(a,(b-1)));
+        }
+}
+Console.Write("Введите число m: "); 
+int m = Convert.ToInt32(Console.ReadLine());  
+Console.Write("Введите число n: "); 
+int n = Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine($"Значение функции Аккермана ({m} , {n}) = {Akkerman(m,n)}");
